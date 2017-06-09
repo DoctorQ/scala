bce9d46d841c6cccecce9017c1d6bb568bb3a6c3
Hui Qian <542113578@qq.com>
2017-06-09 17:06:51 +0800

    define construct

---
+++ src/main/scala/day1/HairColorTrait.scala
@@ -0,0 +1,9 @@
+package day1
+
+/**
+  * Created by doctorq on 2017/6/9.
+  * 特质，trait
+  */
+trait HairColorTrait {
+
+}
--- src/main/scala/day1/Test.scala
+++ src/main/scala/day1/Test.scala
@@ -8,7 +8,30 @@ import com.doctorq.day1.User
 object Test {
   def main(args: Array[String]): Unit = {
     println("Hello,World!")
-
     val usr1 = new User
+    println("=====")
+    val point1 = new Point()
+    println(point1.x)
+    println(point1.y)
+    point1.x = 99
+    point1.y = 101
+    println(point1.x)
+    println(point1.y)
+
+    println("=====")
+
+
+
+    println(point1.x)
+    println(point1)
+
+    val origin = new Point2
+    //第一个参数赋值为1，第二个参数使用默认值
+    val point2 = new Point2(1)
+    println(point2.x)
+
+    val point3 = new Point2(y = 2)
+
+    println(point3.y)
   }
 }
