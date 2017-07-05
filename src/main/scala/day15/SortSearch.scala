53c08c5aeecf73d6efc3fc5ad6052662026dad38
qianhui <qianhui_iwm@iwaimai.baidu.com>
2017-07-05 21:22:55 +0800

    线性查找又名顺序查找

--- src/main/scala/day15/SortSearch.scala
+++ src/main/scala/day15/SortSearch.scala
@@ -8,7 +8,7 @@ object SortSearch extends App {
 
   def search(array: Array[Int], find: Int): Unit = {
     if (array.size == 0) return
-    println(s"I want to find ${find} : ")
+    println(s"I want to find ${find}: ")
     val findArray = for (i <- 0 until array.length if (array(i) == find)) yield array(i)
     if (findArray.size != 0) {
       println(s"yes, I find it")
