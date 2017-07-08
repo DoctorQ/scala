e2c3eab293589355a53da79f48050bdf8ca8fbb3
Hui Qian <542113578@qq.com>
2017-07-08 10:17:43 +0800

    插值排序采用迭代实现

--- src/main/scala/day15/InsertSearch.scala
+++ src/main/scala/day15/InsertSearch.scala
@@ -15,34 +15,28 @@ object InsertSearch extends App with Utils {
   def search(unSorted: Array[Int], find: Int): Unit = {
     val size = unSorted.size
     if (size == 0) {
-      println(s"${find} no find")
+      println(s"empty array")
       return
     }
     printlnArray(unSorted)
     val index =
       (size - 1) * (find - unSorted(0)) / (unSorted(size - 1) - unSorted(0))
-
-    val flag = unSorted(index)
-
-    if (find == flag) {
-      println(s"find ${index}")
-      return
-    } else if (find > flag) {
-      val right = subArray(unSorted, index + 1, size - 1)
-      search(right, find)
-    } else {
-      val left = subArray(unSorted, 0, index - 1)
-      search(left, find)
+    var start = 0
+    var end = size - 1
+    while (start <= end) {
+      val index = end * (find - start) / unSorted(end) - unSorted(start)
+      if (unSorted(index) > find) {
+        end = index - 1
+      } else if (unSorted(index) < find) {
+        start = index + 1
+      } else {
+        println(s"${find} find in index: ${index} ")
+        return
+      }
     }
+    println(s"${find} no found")
   }
 
-  def subArray(parent: Array[Int], start: Int, end: Int): Array[Int] = {
-    val subArray = new ArrayBuffer[Int]()
-    for (i <- start to end) {
-      subArray.append(parent(i))
-    }
-    subArray.toArray
-  }
 
   val list = Array(1, 3, 4, 2, 5, 5, 6, 9, 3, 4, 4)
 
