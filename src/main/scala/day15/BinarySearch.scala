f66af3fdb0100999906f7fbabe067b7037497c79
Hui Qian <542113578@qq.com>
2017-07-08 10:05:01 +0800

    二分查找改为迭代实现

--- src/main/scala/day15/BinarySearch.scala
+++ src/main/scala/day15/BinarySearch.scala
@@ -10,40 +10,39 @@ import scala.collection.mutable.ArrayBuffer
   */
 object BinarySearch extends Utils with App {
 
-
+  /**
+    * 迭代实现
+    *
+    * @param unSorted
+    * @param find
+    */
   def search(unSorted: Array[Int], find: Int): Unit = {
     val size = unSorted.size
     if (size == 0) {
-      println(s"${find} no find")
+      println(s"empty array")
       return
     }
 
     printlnArray(unSorted)
-    val index = size / 2
-    val flag = unSorted(index)
-
-    if (find == flag) {
-      println(s"find ${index}")
-      return
-    } else if (find > flag) {
-      val right = subArray(unSorted, index + 1, size - 1)
-      search(right, find)
-    } else {
-      val left = subArray(unSorted, 0, index - 1)
-      search(left, find)
+    var start = 0
+    var end = size - 1
+    while (start <= end) {
+      val index = (start + end) / 2
+      if (unSorted(index) > find) {
+        end = index - 1
+      } else if (unSorted(index) < find) {
+        start = index + 1
+      } else {
+        println(s"${find} in index : ${index}")
+        return
+      }
     }
-  }
 
-  def subArray(parent: Array[Int], start: Int, end: Int): Array[Int] = {
-    val subArray = new ArrayBuffer[Int]()
-    for (i <- start to end) {
-      subArray.append(parent(i))
-    }
-    subArray.toArray
+    println(s"${find} no found")
   }
 
   val list = Array(1, 3, 4, 2, 5, 5, 6, 9, 3, 4, 4)
 
-  search(list.sorted, 9)
+  search(list.sorted, 8)
 
 }
