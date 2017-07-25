24db26c329935eab40e8fee0f2d0fa6228c679da
Hui Qian <542113578@qq.com>
2017-07-25 23:34:55 +0800

    ZigZag Conversion

--- src/main/scala/day17/LeetCode6.scala
+++ src/main/scala/day17/LeetCode6.scala
@@ -1,5 +1,32 @@
 package day17
 
+/**
+  * https://leetcode.com/problems/zigzag-conversion/#/description
+  */
 object LeetCode6 {
+  def convert(s: String, numRows: Int): String = {
+    if (numRows < 2) return s
+    val length = s.length
+    var result = ""
+    //周期
+    val r = 2 * numRows - 2
+    for (i <- 0 until numRows) {
+      var j = i
+      while (j < length) {
+        result += s.charAt(j)
+        //非首尾行要进行追加
+        if (i > 0 && i < numRows - 1 && (j + r - 2 * i) < length) {
+          result += s.charAt(j + r - 2 * i)
+        }
+        j += r
+      }
+    }
+    result
+  }
 
+
+  def main(args: Array[String]): Unit = {
+    val s = "PAYPALISHIRING"
+    println(convert(s, 3))
+  }
 }
