7bec6821885229afb6310ca3274b35402a551a8f
Hui Qian <542113578@qq.com>
2017-07-27 22:49:08 +0800

    Reverse Integer：数字翻转

---
+++ src/main/scala/day17/LeetCode7.scala
@@ -0,0 +1,28 @@
+package day17
+
+/**
+  * 数字反转
+  */
+object LeetCode7 {
+
+  def reverse(x: Int): Int = {
+    var result: Double = 0
+    var a = if (x < 0) -1 else 1
+    var temp = Math.abs(x)
+    while (temp > 0) {
+      result = result * 10 + temp % 10
+      temp = temp / 10
+    }
+    result *= a
+    if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) 0 else {
+      result.toInt
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val x = 1534236469
+    println(Integer.MAX_VALUE)
+    println(reverse(x))
+  }
+
+}
